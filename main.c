734075e6bd2edf785d191d57df0b66a6c3a29257
tayou89 <ysh00718@gmail.com>
2023-02-26 16:20:39 +0900

    23/02/26 16:20

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tayou <tayou@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 15:21:31 by tayou             #+#    #+#             */
-/*   Updated: 2023/02/26 15:02:35 by tayou            ###   ########.fr       */
+/*   Updated: 2023/02/26 16:19:45 by tayou            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /*
@@ -83,11 +83,40 @@ void	free_array(char **array)
 	free(array);
 }
 
+int	get_array_size(char *str, int *i)
+{
+	int	size;
+	int	start_point;
+	int	end_point;
+
+	start_point = *i;
+	pass_isspace(str, i);
+	pass_notspace(str, i);
+	end_point = *i - 1;
+	size = end_point - start_point + 1;
+	return (size);
+}
+
+char	*fill_array(char *array, char *str, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		array[i] = str[i];
+		i++;
+	}
+	array[i] = '\0';
+	return (array);
+}
+
 char	**malloc_array(char **array, char **argv)
 {
 	int	i;
 	int	j;
 	int	k;
+	int	size;
 
 	k = 0;
 	i = 1;
@@ -96,21 +125,20 @@ char	**malloc_array(char **array, char **argv)
 		j = 0;
 		while (argv[i][j] != '\0')
 		{
-			pass_isspace(argv[i], &j);
-			pass_notspace(argv[i], &j);
-			array[k] = (char *) malloc(sizeof(char) * j + 1);
+			size = get_array_size(argv[i], &j);
+			array[k] = (char *) malloc(sizeof(char) * size + 1);
 			if (array[k] == 0)
 			{
 				free_array(array);
 				return (0);
 			}
+			array[k] = fill_array(array[k], &argv[i][j-size], size);
 			k++;
 		}
 		i++;
 	}
 	return (array);
 }
-
 
 int	main(int argc, char *argv[])
 {
